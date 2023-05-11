eb208674618c9f625ac6ccaf9e70bc55d3dd9014
yamil-90 <yamil.coloma90@gmail.com>
2023-05-11 12:18:06 -0300

    settings changes

--- videoSite/settings.py
+++ videoSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from .database_info import database_info
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,12 +97,7 @@ WSGI_APPLICATION = 'videoSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = database_info
 
 
 # Password validation
